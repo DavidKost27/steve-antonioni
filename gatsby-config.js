565fc6b94ebdc9c399d8add66c75eb49f7533cf5
David Kostuchenko <davidkost1999@gmail.com>
2021-01-26 07:46:39 +0200

    [Update]Managed to setup the plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,21 @@ module.exports = {
   siteMetadata: {
     title: "Steve Antonioni",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-google-fonts-v2`,
+      options: {
+        fonts: [
+          {
+            family: "Rancho",
+          },
+          {
+            family: "Kaushan+Script",
+          },
+        ],
+      },
+    },
+    // other plugins
+  ],
 };
